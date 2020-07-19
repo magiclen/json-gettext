21b36889349d1c88e576d80e5302348b59342f62
Magic Len <len@magiclen.org>
2020-07-20 01:14:43 +0800

    Key supports Rocket now

--- src/key_copy/rocket_feature/mod.rs
+++ src/key_copy/rocket_feature/mod.rs
@@ -1,11 +1,39 @@
+extern crate rocket;
+
 #[cfg(debug_assertions)]
 mod debug;
 
 #[cfg(not(debug_assertions))]
 mod release;
 
+use std::str::FromStr;
+use std::error::Error;
+
 #[cfg(debug_assertions)]
 pub use debug::*;
 
 #[cfg(not(debug_assertions))]
 pub use release::*;
+
+use rocket::http::RawStr;
+use rocket::request::{FromFormValue, FromParam};
+
+use super::Key;
+
+impl<'a> FromFormValue<'a> for Key {
+    type Error = Box<dyn Error>;
+
+    #[inline]
+    fn from_form_value(v: &'a RawStr) -> Result<Self, Self::Error> {
+        Ok(Key::from_str(v)?)
+    }
+}
+
+impl<'a> FromParam<'a> for Key {
+    type Error = Box<dyn Error>;
+
+    #[inline]
+    fn from_param(v: &'a RawStr) -> Result<Self, Self::Error> {
+        Ok(Key::from_str(v)?)
+    }
+}
--- src/key_string/rocket_feature/mod.rs
+++ src/key_string/rocket_feature/mod.rs
@@ -1,3 +1,5 @@
+extern crate rocket;
+
 #[cfg(debug_assertions)]
 mod debug;
 
@@ -9,3 +11,26 @@ pub use debug::*;
 
 #[cfg(not(debug_assertions))]
 pub use release::*;
+
+use rocket::http::RawStr;
+use rocket::request::{FromFormValue, FromParam};
+
+use super::Key;
+
+impl<'a> FromFormValue<'a> for Key {
+    type Error = ();
+
+    #[inline]
+    fn from_form_value(v: &'a RawStr) -> Result<Self, Self::Error> {
+        Ok(Key(String::from(v.as_str())))
+    }
+}
+
+impl<'a> FromParam<'a> for Key {
+    type Error = ();
+
+    #[inline]
+    fn from_param(v: &'a RawStr) -> Result<Self, Self::Error> {
+        Ok(Key(String::from(v.as_str())))
+    }
+}
