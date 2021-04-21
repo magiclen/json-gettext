5f08a471b75f3cbbd08afdbfdab4c7a0b67d4563
Magic Len <len@magiclen.org>
2021-04-22 06:40:32 +0800

    update workflows and examples

--- examples/hello.rs
+++ examples/hello.rs
@@ -1,37 +1,48 @@
-#![feature(proc_macro_hygiene, decl_macro)]
+// TODO: Need mutually exclusive features to remove those cfg attributes below.
 
+#[cfg(not(any(feature = "language", feature = "region", feature = "language_region_pair")))]
 #[macro_use]
 extern crate rocket;
 
+#[cfg(not(any(feature = "language", feature = "region", feature = "language_region_pair")))]
 #[macro_use]
 extern crate json_gettext;
 
+#[cfg(not(any(feature = "language", feature = "region", feature = "language_region_pair")))]
 use rocket::response::Redirect;
+
+#[cfg(not(any(feature = "language", feature = "region", feature = "language_region_pair")))]
 use rocket::State;
 
+#[cfg(not(any(feature = "language", feature = "region", feature = "language_region_pair")))]
 use json_gettext::JSONGetTextManager;
 
+#[cfg(not(any(feature = "language", feature = "region", feature = "language_region_pair")))]
 #[get("/")]
 fn index(ctx: State<JSONGetTextManager>) -> Redirect {
     Redirect::temporary(uri!(hello: lang = ctx.get_default_key()))
 }
 
+#[cfg(not(any(feature = "language", feature = "region", feature = "language_region_pair")))]
 #[get("/<lang>")]
 fn hello(ctx: State<JSONGetTextManager>, lang: String) -> String {
     format!("Ron: {}", get_text!(ctx, lang, "hello").unwrap().as_str().unwrap())
 }
 
 fn main() {
-    rocket::ignite()
-        .attach(JSONGetTextManager::fairing(|| {
-            static_json_gettext_build_for_rocket!(
-                "en_US",
-                "en_US",
-                "langs/en_US.json",
-                "zh_TW",
-                "langs/zh_TW.json"
-            )
-        }))
-        .mount("/", routes![index, hello])
-        .launch();
+    #[cfg(not(any(feature = "language", feature = "region", feature = "language_region_pair")))]
+    {
+        rocket::ignite()
+            .attach(JSONGetTextManager::fairing(|| {
+                static_json_gettext_build_for_rocket!(
+                    "en_US",
+                    "en_US",
+                    "langs/en_US.json",
+                    "zh_TW",
+                    "langs/zh_TW.json"
+                )
+            }))
+            .mount("/", routes![index, hello])
+            .launch();
+    }
 }
